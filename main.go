a3641e32941a920d767b3ee3b2241b07f6dec155
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2021-06-06 15:34:39 +0200

    Add defer coroutine multi pinout for triac

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
         "strconv"
 	"github.com/warthog618/gpio"
         "strings"
+	"sync"
 )
-
 
 func getInt(key string) uint32 {
     //s := os.Getenv(key)
@@ -27,34 +27,46 @@ func getInt(key string) uint32 {
         signals = 1000
       }
 
-      if signals >= 9000 {
-	signals = 9000
-      }
+      // if signals >= 9000 {
+      // 	signals = 8700
+      // }
       return uint32(signals)
     }
     return 0
 }
 
-func getenvInt(key string) uint32 {
-    s := os.Getenv(key)
-    if signals, err := strconv.ParseFloat(s, 32); err == nil {
-			if signals < 100 {
-        signals = 9000-(signals*80)
-      } else if (signals < 9000) && (signals > 1000) {
-        signals = (signals)
-      }
+func pwm2defer(wg *sync.WaitGroup, percent2 uint32) {
+	if os.Getenv("pwmSTOP") == "True" {
+		gpio.Close()
+		os.Exit(0)
+	}
+	var pinPWM2 = gpio.NewPin(gpio.GPIO20)
+	pinPWM2.Output()
 
-      if signals < 1000 {
-	signals = 1000
-      }
+	defer wg.Done()
+	if ((percent2 >= 1) && (percent2 < 10000))  {
+		time.Sleep((time.Duration(percent2) * time.Microsecond))
+		pinPWM2.High()
+		time.Sleep((time.Duration(5) * time.Microsecond))
+		pinPWM2.Low()
+	}
+}
 
-      if signals >= 9000 {
-	 signals = 9000
-      }
+func pwm1defer(wg *sync.WaitGroup, percent1 uint32) {
+	if os.Getenv("pwmSTOP") == "True" {
+			gpio.Close()
+			os.Exit(0)
+	}
+	var pinPWM1 = gpio.NewPin(gpio.GPIO16)
+	pinPWM1.Output()
+	defer wg.Done()
+	if ((percent1 >= 1) && (percent1 < 10000))  {
+		time.Sleep((time.Duration(percent1) * time.Microsecond))
+		pinPWM1.High()
+		time.Sleep((time.Duration(5) * time.Microsecond))
+		pinPWM1.Low()
+	}
 
-      return uint32(signals)
-    }
-    return 0
 }
 
 func pwm(pins int, pins2 int) (string, error) {
@@ -63,49 +75,69 @@ func pwm(pins int, pins2 int) (string, error) {
   if err != nil {
     panic(err)
   }
+
   defer gpio.Close()
+
   pin := gpio.NewPin(gpio.GPIO6)
   pin.Input()
   pin.PullUp()
-
-  pinPWM1 := gpio.NewPin(pins)
-  pinPWM1.Output()
-
-  pinPWM2 := gpio.NewPin(pins2)
-  pinPWM2.Output()
 
   // capture exit signals to ensure resources are released on exit.
   quit := make(chan os.Signal, 1)
   signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
   defer signal.Stop(quit)
 
-  percent1 := uint32(9000)
-  percent2 := uint32(9000)
+  percent1 := uint32(0)
+  percent2 := uint32(0)
 
   fmt.Println("init pwm process...")
 
   err = pin.Watch(gpio.EdgeBoth, func(pin *gpio.Pin) {
-    if pin.Read() == true {
-        if (percent1 > 0) {
-          time.Sleep((time.Duration(percent1) * time.Microsecond))
-          pinPWM1.High()
-        }
-        if (percent2 > 0) {
-          time.Sleep((time.Duration(percent2) * time.Microsecond))
-          pinPWM2.High()
-        }
-    }
+    //if pin.Read() == true {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go pwm1defer(&wg, percent1)
+	go pwm2defer(&wg, percent2)
+	wg.Wait()
 
-    if pin.Read() == false {
-      if (percent1 > 0) {
-        time.Sleep((time.Duration(percent1) * time.Microsecond))
-        pinPWM1.Low()
-      }
-      if (percent2 > 0) {
-        time.Sleep((time.Duration(percent2) * time.Microsecond))
-        pinPWM2.Low()
-      }
-    }
+        // if ((percent1 >= 1) && (percent1 < 10000))  {
+        //   time.Sleep((time.Duration(percent1) * time.Microsecond))
+        //   pinPWM1.High()
+        //   time.Sleep((time.Duration(5) * time.Microsecond))
+        //   pinPWM1.Low()
+        // }
+				//
+        // if ((percent2 >= 1) && (percent2 < 10000)) {
+        //   time.Sleep((time.Duration(percent2) * time.Microsecond))
+        //   pinPWM2.High()
+        //   time.Sleep((time.Duration(5) * time.Microsecond))
+        //   pinPWM2.Low()
+        // }
+  //  }
+
+    // if pin.Read() == false {
+    //   if ((percent1 >= 1) && (percent1 < 10000)) {
+    //     time.Sleep((time.Duration(percent1) * time.Microsecond))
+    //     pinPWM1.High()
+    //     time.Sleep((time.Duration(5) * time.Nanosecond))
+    //     pinPWM1.Low()
+    //   }
+    //   if ((percent2 >= 1) && (percent2 < 10000)) {
+    //     time.Sleep((time.Duration(percent2) * time.Microsecond))
+    //     pinPWM2.High()
+    //     time.Sleep((time.Duration(5) * time.Nanosecond))
+    //     pinPWM2.Low()
+    //   }
+    // }
+
+		// if ((percent1 == 10000) || (percent1 == 0)) {
+    //   pinPWM1.Low()
+		// }
+    //
+		// if ((percent2 == 10000) || (percent2 == 0)) {
+    //   pinPWM2.Low()
+		// }
+
   })
 
   if err != nil {
@@ -119,37 +151,44 @@ func pwm(pins int, pins2 int) (string, error) {
   fmt.Println("please wait command usage :\n cmd : pwm[1-2] percent[0-100%]\n cmd : [reset] for disable all comand \n cmd : [stop] for quit program ")
   input := bufio.NewScanner(os.Stdin)
   for input.Scan() {
-    if input.Text() == "stop" {
-	percent1 = uint32(9000)
-	percent2 = uint32(9000)
-        break
+
+    fmt.Println(os.Getenv("pwmSTOP"))
+
+    if input.Text() == "stop" || os.Getenv("pwmSTOP") == "True" {
+	percent1 = uint32(0)
+	percent2 = uint32(0)
+	gpio.Close()
+	break
     } else {
 
     if strings.Contains(input.Text(), "pwm1") {
-        detectpercent := strings.Split(input.Text(), " ")[1]
-        percent1 = getInt(detectpercent)
-        if percent1 == 10000 {
-          percent1 = 1
-        }
-        if percent1 == 0 {
+        detectpercent1 := strings.Split(input.Text(), " ")[1]
+        percent1 = getInt(detectpercent1)
+	if ((percent1 == 8999) || (percent1 == 9000)) {
           percent1 = 0
         }
-	fmt.Println("debug:", percent1)
+	// if percent1 == 0 {
+        //   percent1 = 0
+        // }
+	// if percent1 >= 95 {
+        //   percent1 = 1000
+	// }
+	fmt.Println("debug pwm1:", percent1)
       }
 
       if strings.Contains(input.Text(), "pwm2") {
-        detectpercent := strings.Split(input.Text(), " ")[1]
-        percent2 = getInt(detectpercent)
-        if percent2 == 10000 {
-          percent2 = 1
-        }
-	if percent2 == 0 {
+        detectpercent2 := strings.Split(input.Text(), " ")[1]
+        percent2 = getInt(detectpercent2)
+        if ((percent2 == 8999) || (percent2 == 9000)) {
           percent2 = 0
         }
-	if percent2 >= 95 {
-          percent2 = 1000
-	}
-        //fmt.Println("debug:", percent2)
+	// if percent2 == 0 {
+        //   percent2 = 0
+        // }
+	// if percent2 >= 95 {
+        //   percent2 = 1000
+				// }
+        fmt.Println("debug pwm2:", percent2)
       }
 
       if strings.Contains(input.Text(), "reset") {
@@ -167,5 +206,5 @@ func pwm(pins int, pins2 int) (string, error) {
 
 // Watches GPIO 4 (J8 7) and reports when it changes state.
 func main() {
-    pwm(gpio.GPIO18, gpio.GPIO19)
+    pwm(gpio.GPIO16, gpio.GPIO20)
 }
